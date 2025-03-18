ccb78837d387aa70f03f70dec51cd4cfe6b67768
pojanZyz <akunword0@gmail.com>
2025-03-18 12:16:28 +0700

    fix() : update

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -6,7 +6,7 @@ const upload = require('../middleware/upload');
 router.get('/', articleController.getArticles);
 router.get('/:id', articleController.getArticleById);
 router.post('/', upload.single('image'), articleController.createArticle);
-router.put('/:id', upload.single('image'), articleController.updateArticle);
+router.patch('/:id', upload.single('image'), articleController.updateArticle);
 router.delete('/:id', articleController.deleteArticle);
 
 module.exports = router;
