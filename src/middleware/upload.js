70aa24b9a8c55bb3def2c62c41f16dc246d23908
pojanZyz <akunword0@gmail.com>
2025-03-18 11:36:21 +0700

    fix() : import image

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,23 +1,6 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const multer = require("multer");
 
-const uploadDir = path.join(__dirname, '../uploads');
-
-// Cek apakah folder uploads ada, jika tidak buat foldernya
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
+const storage = multer.memoryStorage(); // Simpan file di memori, bukan di disk
+const upload = multer({ storage });
 
 module.exports = upload;
