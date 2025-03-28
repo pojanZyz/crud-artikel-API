96cb087fd870389afb832a3144ed8cf529cda808
pojanZyz <akunword0@gmail.com>
2025-03-28 16:29:21 +0700

    fix

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -3,4 +3,4 @@ const multer = require("multer");
 const storage = multer.memoryStorage(); // Simpan file di memori, bukan di disk
 const upload = multer({ storage });
 
-module.exports = upload;
+module.exports = {upload};
--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const articleController = require('../controller/articleControl');
-const upload = require('../middleware/upload');
+const {upload} = require('../middleware/upload');
 
 router.get('/', articleController.getArticles);
 router.get('/:id', articleController.getArticleById);
